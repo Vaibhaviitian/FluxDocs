81a1360268682ce01007f4028e5d3731ed13cd56
Vaibhaviitian <aryanvaibhav74@gmail.com>
2024-10-12 02:42:46 +0530

    Adding basic ui lib like shade and magic ui and tailwind too

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "@/components/ui/button";
+import Particles from "@/components/ui/particles";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Particles className="h-screen w-screen bg-black relative"></Particles>
+      <div className="absolute inset-0 text-white z-10 p-5 text-center ">
+        VAIBHAV READY FOR SETUP
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="absolute inset-0 text-3xl m-5 text-yellow-500 z-10 p-5 text-center">
+        FLUXDOCS
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
-
-export default App
